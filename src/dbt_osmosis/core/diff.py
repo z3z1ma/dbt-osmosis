ff17a33e7e500062c031c278c2c43bda73e66a47
z3z1ma <butler.alex2010@gmail.com>
2022-08-05 16:13:47 -0700

    use dbtonic node insertion for git diff

--- src/dbt_osmosis/core/diff.py
+++ src/dbt_osmosis/core/diff.py
@@ -1,5 +1,5 @@
 import hashlib
-import tempfile
+import uuid
 from pathlib import Path
 from typing import Tuple
 
@@ -13,84 +13,83 @@ from dbt_osmosis.core.osmosis import DbtOsmosis
 
 def build_diff_queries(model: str, runner: DbtOsmosis) -> Tuple[str, str]:
     """Leverage git to build two temporary tables for diffing the results of a query throughout a change"""
-    node = runner.dbt.ref_lookup.find(model, package=None, manifest=runner.dbt)
+    # Resolve git node
+    node = runner.get_ref_node(model)
     dbt_path = Path(node.root_path)
-    abs_path = dbt_path / node.original_file_path
     repo = Repo(dbt_path, search_parent_directories=True)
     t = next(Path(repo.working_dir).rglob(node.original_file_path)).relative_to(repo.working_dir)
     target = repo.head.object.tree[str(t)]
-    with tempfile.NamedTemporaryFile(delete=True, dir=str(abs_path.parent), suffix=".sql") as f:
-        f.write(target.data_stream.read())
-        f.flush()
-        runner.rebuild_dbt_manifest()
-        original_node = runner.dbt.ref_lookup.find(
-            Path(f.name).stem, package=None, manifest=runner.dbt
-        )  # Injected Original Temp Node --> Identifier resolved ot commit hash at runtime
-        changed_node = runner.dbt.ref_lookup.find(model, package=None, manifest=runner.dbt)
-        with runner.adapter.connection_named("dbt-osmosis"):
-            # Original Node Resolution
-            original_node = runner.adapter.get_compiler().compile_node(original_node, runner.dbt)
-            # Diff Node Resolution
-            changed_node = runner.adapter.get_compiler().compile_node(changed_node, runner.dbt)
+
+    # Create original node
+    git_node_name = "z" + str(uuid.uuid4()).replace("-", "")
+    original_node = runner.sql_parser.parse_remote(target.data_stream.read(), git_node_name)
+
+    # Alias changed node
+    changed_node = node
+
+    # Compile models
+    original_node = runner.compile_node(original_node)
+    changed_node = runner.compile_node(changed_node)
 
     return original_node.compiled_sql, changed_node.compiled_sql
 
 
 def build_diff_tables(model: str, runner: DbtOsmosis) -> Tuple[BaseRelation, BaseRelation]:
     """Leverage git to build two temporary tables for diffing the results of a query throughout a change"""
-    node = runner.dbt.ref_lookup.find(model, package=None, manifest=runner.dbt)
+    # Resolve git node
+    node = runner.get_ref_node(model)
     dbt_path = Path(node.root_path)
-    abs_path = dbt_path / node.original_file_path
     repo = Repo(dbt_path, search_parent_directories=True)
     t = next(Path(repo.working_dir).rglob(node.original_file_path)).relative_to(repo.working_dir)
     sha = repo.head.object.hexsha
     target = repo.head.object.tree[str(t)]
-    with tempfile.NamedTemporaryFile(delete=True, dir=str(abs_path.parent), suffix=".sql") as f:
-        f.write(target.data_stream.read())
-        f.flush()
-        runner.rebuild_dbt_manifest()
-        original_node = runner.dbt.ref_lookup.find(
-            Path(f.name).stem, package=None, manifest=runner.dbt
-        )  # Injected Original Temp Node --> Identifier resolved ot commit hash at runtime
-        changed_node = runner.dbt.ref_lookup.find(model, package=None, manifest=runner.dbt)
+
+    # Create original node
+    git_node_name = "z_" + sha[-5:]
+    original_node = runner.sql_parser.parse_remote(target.data_stream.read(), git_node_name)
+
+    # Alias changed node
+    changed_node = node
+
+    # Compile models
+    original_node = runner.compile_node(original_node)
+    changed_node = runner.compile_node(changed_node)
+
+    # Lookup and resolve original ref based on git sha
+    git_node_parts = original_node.database, original_node.schema, git_node_name
+    check_ref = runner.adapter.get_relation(*git_node_parts)
+    if not check_ref:
+        ref_A = runner.adapter.Relation.create(*git_node_parts)
+        logger().info("Creating new relation for %s", ref_A)
         with runner.adapter.connection_named("dbt-osmosis"):
-            # Original Node Resolution
-            original_node = runner.adapter.get_compiler().compile_node(original_node, runner.dbt)
-            _ref = runner.adapter.get_relation(original_node.database, original_node.schema, sha)
-            if not _ref:
-                ref_A = runner.adapter.Relation.create(
-                    original_node.database, original_node.schema, sha
-                )
-                logger().info("Creating new relation for %s", ref_A)
-                runner.execute_macro(
-                    "create_table_as",
-                    kwargs={
-                        "sql": original_node.compiled_sql,
-                        "relation": ref_A,
-                        "temporary": True,
-                    },
-                    run_compiled_sql=True,
-                )
-            else:
-                ref_A = _ref
-                logger().info("Found existing relation for %s", ref_A)
-            # Diff Node Resolution
-            changed_node = runner.adapter.get_compiler().compile_node(changed_node, runner.dbt)
-            ref_B = runner.adapter.Relation.create(
-                changed_node.database,
-                changed_node.schema,
-                hashlib.md5(changed_node.identifier.encode("utf-8")).hexdigest(),
-            )
-            logger().info("Creating new relation for %s", ref_B)
             runner.execute_macro(
                 "create_table_as",
                 kwargs={
-                    "sql": changed_node.compiled_sql,
-                    "relation": ref_B,
+                    "sql": original_node.compiled_sql,
+                    "relation": ref_A,
                     "temporary": True,
                 },
                 run_compiled_sql=True,
             )
+    else:
+        ref_A = check_ref
+        logger().info("Found existing relation for %s", ref_A)
+
+    # Resolve modified fake ref
+    temp_node_name = "z_" + hashlib.md5(changed_node.identifier.encode("utf-8")).hexdigest()
+    ref_B = runner.adapter.Relation.create(
+        changed_node.database, changed_node.schema, temp_node_name
+    )
+    logger().info("Creating new relation for %s", ref_B)
+    runner.execute_macro(
+        "create_table_as",
+        kwargs={
+            "sql": changed_node.compiled_sql,
+            "relation": ref_B,
+            "temporary": True,
+        },
+        run_compiled_sql=True,
+    )
 
     return ref_A, ref_B
 
@@ -101,7 +100,7 @@ def diff_tables(
 
     logger().info("Running diff")
     _, _, table = runner.execute_macro(
-        "compare_relations",
+        "_dbt_osmosis_compare_relations",
         kwargs={
             "a_relation": ref_A,
             "b_relation": ref_B,
@@ -119,7 +118,7 @@ def diff_queries(
 
     logger().info("Running diff")
     _, _, table = runner.execute_macro(
-        "compare_queries" if aggregate else "dbt_osmosis_compare",
+        "_dbt_osmosis_compare_queries_agg" if aggregate else "_dbt_osmosis_compare_queries",
         kwargs={
             "a_query": sql_A,
             "b_query": sql_B,
