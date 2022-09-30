70f219fe4cc32b3975331c2b303dd99101d76565
z3z1ma <butler.alex2010@gmail.com>
2022-09-30 13:51:48 -0700

    ✨ multi project support with register endpoints

--- src/dbt_osmosis/core/server.py
+++ src/dbt_osmosis/core/server.py
@@ -1,16 +1,52 @@
+"""Server Interface Notes
+Executing `dbt-osmosis server` will seed a "default" project based on
+initially supplied --project-dir / --profiles-dir args
+TODO: we will probably gut this and migrate it all to Fast API in the near-term
+
+/run [?limit=...]
+    POST
+    Header: X-dbt-Project=projectName Content-Type=text/plain
+    Body: dbt SQL statement
+    Action: Executes SQL (uses default project if X-dbt-Project not supplied)
+
+/compile
+    POST
+    Header: X-dbt-Project=projectName Content-Type=text/plain
+    Body: dbt SQL statement
+    Action: Compiles SQL (uses default project if X-dbt-Project not supplied)
+
+/parse [?target=...&reset=true/false]
+    GET
+    Header: X-dbt-Project
+    reparses project and writes manifest.json (uses default project if X-dbt-Project not supplied)
+
+/register
+    POST
+    Header: X-dbt-Project [required] Content-Type=application/json
+    adds new project runner
+
+/unregister 
+    POST
+    Header: X-dbt-Project [required] Content-Type=application/json
+    removes project runner
+"""
 import datetime
 import decimal
 import re
 import threading
 import time
 import uuid
+from enum import Enum
+from typing import Any, Dict, List, Union
 
 import orjson
-from bottle import JSONPlugin, install, request, response, route, run, uninstall
+from bottle import JSONPlugin, install, request, response, route, run
+from pydantic import BaseModel
 
 from dbt_osmosis.core.log_controller import logger
 from dbt_osmosis.core.osmosis import DbtOsmosis
 
+DEFAULT = "__default__"
 JINJA_CH = ["{{", "}}", "{%", "%}"]
 MUTEX = threading.Lock()
 
@@ -21,17 +57,67 @@ def default(obj):
     raise TypeError
 
 
+class OsmosisRunResult(BaseModel):
+    column_names: List[str]
+    rows: List[List[Any]]
+    raw_sql: str
+    compiled_sql: str
+
+
+class OsmosisCompileResult(BaseModel):
+    result: str
+
+
+class OsmosisResetResult(BaseModel):
+    result: str
+
+
+class OsmosisRegisterResult(BaseModel):
+    added: str
+    projects: List[str]
+
+
+class OsmosisUnregisterResult(BaseModel):
+    removed: str
+    projects: List[str]
+
+
+class OsmosisErrorCode(Enum):
+    FailedToReachServer = -1
+    CompileSqlFailure = 1
+    ExecuteSqlFailure = 2
+    ProjectParseFailure = 3
+    ProjectNotRegistered = 4
+    ProjectHeaderNotSupplied = 5
+
+
+class OsmosisError(BaseModel):
+    code: OsmosisErrorCode
+    message: str
+    data: Dict[str, Any]
+
+
+class OsmosisErrorContainer(BaseModel):
+    error: OsmosisError
+
+
 class DbtOsmosisPlugin:
     name = "dbt-osmosis"
     api = 2
 
     def __init__(self, runner: DbtOsmosis):
-        self.runner = runner
+        self.runners: Dict[str, DbtOsmosis] = {
+            DEFAULT: runner,
+            runner.config.project_name: runner,
+        }
 
     def apply(self, callback, route):
         def wrapper(*args, **kwargs):
             start = time.time()
-            body = callback(*args, **kwargs, runner=self.runner)
+            # Headers are read-only so we can't inject it, handle it at the route level
+            # if not request.get_header("X-dbt-Project"):
+            #     request.headers["X-dbt-Project"] = self.runners[DEFAULT].config.project_name
+            body = callback(*args, **kwargs, runners=self.runners)
             end = time.time()
             response.headers["X-dbt-Exec-Time"] = str(end - start)
             return body
@@ -40,68 +126,132 @@ class DbtOsmosisPlugin:
 
 
 @route("/run", method="POST")
-def run_sql(runner: DbtOsmosis):
-    query = f'\n{request.body.read().decode("utf-8").strip()}\n'
+def run_sql(runners: Dict[str, DbtOsmosis]) -> Union[OsmosisRunResult, OsmosisErrorContainer, str]:
+    # Project Support
+    project = request.get_header("X-dbt-Project", DEFAULT)
+    project_runner = runners.get(project)
+    if project != DEFAULT and not project_runner:
+        return OsmosisErrorContainer(
+            error=OsmosisError(
+                code=OsmosisErrorCode.ProjectNotRegistered,
+                message="Project is not registered. Make a POST request to the /register endpoint first to register a runner",
+                data={"registered_projects": runners.keys()},
+            )
+        ).json()
+    elif not project_runner:
+        project_runner = runners[DEFAULT]
+
+    # Query Construction
+    query = f'\n(\n{{# PAD #}}{request.body.read().decode("utf-8").strip()}{{# PAD #}}\n)\n'
     limit = request.query.get("limit", 200)
-    query_with_limit = f"select * from ({query}) as osmosis_query limit {limit}"
+    query_with_limit = (
+        # we need to support `TOP` too
+        f"select * from ({query}) as osmosis_query limit {limit}"
+    )
+
     try:
-        result = runner.execute_sql(query_with_limit)
-    except Exception as exc:
-        return {"error": {"code": 2, "message": str(exc), "data": exc.__dict__}}
+        result = project_runner.execute_sql(query_with_limit)
+    except Exception as execution_err:
+        return OsmosisErrorContainer(
+            error=OsmosisError(
+                code=OsmosisErrorCode.ExecuteSqlFailure,
+                message=str(execution_err),
+                data=execution_err.__dict__,
+            )
+        ).json()
+
+    # Re-extract compiled query and return data structure
     compiled_query = re.search(
         r"select \* from \(([\w\W]+)\) as osmosis_query", result.node.compiled_sql
     ).groups()[0]
-    return {
-        "rows": [list(row) for row in result.table.rows],
-        "column_names": result.table.column_names,
-        "compiled_sql": compiled_query,
-        "raw_sql": query,
-    }
+    return OsmosisRunResult(
+        rows=[list(row) for row in result.table.rows],
+        column_names=result.table.column_names,
+        compiled_sql=compiled_query.strip()[1:-1],
+        raw_sql=query,
+    ).json()
 
 
 @route("/compile", method="POST")
-def compile_sql(runner: DbtOsmosis):
-    query = request.body.read().decode("utf-8").strip()
+def compile_sql(
+    runners: Dict[str, DbtOsmosis]
+) -> Union[OsmosisCompileResult, OsmosisErrorContainer, str]:
+    # Project Support
+    project = request.get_header("X-dbt-Project", DEFAULT)
+    project_runner = runners.get(project)
+    if project != DEFAULT and not project_runner:
+        ...  # requires registration...
+    elif not project_runner:
+        project_runner = runners[DEFAULT]
+
+    # Query Compilation
+    query: str = request.body.read().decode("utf-8").strip()
     if any(CH in query for CH in JINJA_CH):
         try:
-            compiled_query = runner.compile_sql(query)
-        except Exception as exc:
-            return {"error": {"code": 1, "message": str(exc), "data": exc.__dict__}}
+            compiled_query = project_runner.compile_sql(query)
+        except Exception as compile_err:
+            return OsmosisErrorContainer(
+                error=OsmosisError(
+                    code=OsmosisErrorCode.CompileSqlFailure,
+                    message=str(compile_err),
+                    data=compile_err.__dict__,
+                )
+            ).json()
     else:
         compiled_query = query
-    return {"result": compiled_query}
+
+    return OsmosisCompileResult(result=compiled_query).json()
 
 
 @route(["/parse", "/reset"])
-def reset(runner: DbtOsmosis):
+def reset(runners: Dict[str, DbtOsmosis]) -> Union[OsmosisResetResult, OsmosisErrorContainer, str]:
+    # Project Support
+    project = request.get_header("X-dbt-Project", DEFAULT)
+    project_runner = runners.get(project)
+    if project != DEFAULT and not project_runner:
+        return OsmosisErrorContainer(
+            error=OsmosisError(
+                code=OsmosisErrorCode.ProjectNotRegistered,
+                message="Project is not registered. Make a POST request to the /register endpoint first to register a runner",
+                data={"registered_projects": runners.keys()},
+            )
+        ).json()
+    elif not project_runner:
+        project_runner = runners[DEFAULT]
+
+    # Determines if we should clear caches and reset config before re-seeding runner
     reset = str(request.query.get("reset", "false")).lower() == "true"
-    old_target = getattr(runner.args, "target", runner.config.target_name)
+
+    # Get targets
+    old_target = getattr(project_runner.args, "target", project_runner.config.target_name)
     new_target = request.query.get("target", old_target)
+
     if not reset and old_target == new_target:
         # Async (target same)
         if MUTEX.acquire(blocking=False):
             logger().debug("Mutex locked")
             parse_job = threading.Thread(
-                target=_reset, args=(runner, reset, old_target, new_target)
+                target=_reset, args=(project_runner, reset, old_target, new_target)
             )
             parse_job.start()
-            return {"result": "Initializing project parsing"}
+            return OsmosisResetResult(result="Initializing project parsing").json()
         else:
             logger().debug("Mutex is locked, reparse in progress")
-            return {"result": "Currently reparsing project"}
+            return OsmosisResetResult(result="Currently reparsing project").json()
     else:
         # Sync (target changed or reset is true)
         if MUTEX.acquire(blocking=old_target != new_target):
             logger().debug("Mutex locked")
-            return _reset(runner, reset, old_target, new_target)
+            return _reset(project_runner, reset, old_target, new_target).json()
         else:
             logger().debug("Mutex is locked, reparse in progress")
-            return {"result": "Currently reparsing project"}
+            return OsmosisResetResult(result="Currently reparsing project").json()
 
 
-def _reset(runner: DbtOsmosis, reset: bool, old_target: str, new_target: str):
+def _reset(
+    runner: DbtOsmosis, reset: bool, old_target: str, new_target: str
+) -> Union[OsmosisResetResult, OsmosisErrorContainer]:
     target_did_change = old_target != new_target
-    rv = {}
     try:
         runner.args.target = new_target
         logger().debug("Starting reparse")
@@ -109,20 +259,91 @@ def _reset(runner: DbtOsmosis, reset: bool, old_target: str, new_target: str):
     except Exception as reparse_err:
         logger().debug("Reparse error")
         runner.args.target = old_target
-        rv["error"] = {"code": 3, "message": str(reparse_err), "data": reparse_err.__dict__}
+        rv = OsmosisErrorContainer(
+            error=OsmosisError(
+                code=OsmosisErrorCode.ProjectParseFailure,
+                message=str(reparse_err),
+                data=reparse_err.__dict__,
+            )
+        )
     else:
         logger().debug("Reparse success")
         runner._version += 1
-        uninstall(DbtOsmosisPlugin), install(DbtOsmosisPlugin(runner=runner))
-        rv["result"] = (
-            f"Profile target changed from {old_target} to {new_target}!"
-            if target_did_change
-            else f"Reparsed project with profile {old_target}!"
+        rv = OsmosisResetResult(
+            result=(
+                f"Profile target changed from {old_target} to {new_target}!"
+                if target_did_change
+                else f"Reparsed project with profile {old_target}!"
+            )
         )
     finally:
         logger().debug("Unlocking mutex")
         MUTEX.release()
     return rv
+
+
+@route("/register", method="POST")
+def register(runners: Dict[str, DbtOsmosis]) -> Union[OsmosisResetResult, OsmosisErrorContainer]:
+    # Project Support
+    project = request.get_header("X-dbt-Project")
+    if not project:
+        return OsmosisErrorContainer(
+            error=OsmosisError(
+                code=OsmosisErrorCode.ProjectHeaderNotSupplied,
+                message="Project header `X-dbt-Project` was not supplied but is required for this endpoint",
+                data=dict(request.headers),
+            )
+        ).json()
+    if project in runners:
+        # Idempotent
+        return OsmosisRegisterResult(added=project, projects=runners.keys())
+
+    # Inputs
+    project_dir = request.json["project_dir"]
+    profiles_dir = request.json["profiles_dir"]
+    target = request.json.get("target")
+
+    try:
+        new_runner = DbtOsmosis(
+            project_dir=project_dir,
+            profiles_dir=profiles_dir,
+            target=target,
+        )
+    except Exception as init_err:
+        return OsmosisErrorContainer(
+            error=OsmosisError(
+                code=OsmosisErrorCode.ProjectParseFailure,
+                message=str(init_err),
+                data=init_err.__dict__,
+            )
+        )
+
+    runners[project] = new_runner
+    return OsmosisRegisterResult(added=project, projects=runners.keys())
+
+
+@route("/unregister", method="POST")
+def register(runners: Dict[str, DbtOsmosis]) -> Union[OsmosisResetResult, OsmosisErrorContainer]:
+    # Project Support
+    project = request.get_header("X-dbt-Project")
+    if not project:
+        return OsmosisErrorContainer(
+            error=OsmosisError(
+                code=OsmosisErrorCode.ProjectHeaderNotSupplied,
+                message="Project header `X-dbt-Project` was not supplied but is required for this endpoint",
+                data=dict(request.headers),
+            )
+        ).json()
+    if project not in runners:
+        return OsmosisErrorContainer(
+            error=OsmosisError(
+                code=OsmosisErrorCode.ProjectNotRegistered,
+                message="Project is not registered. Make a POST request to the /register endpoint first to register a runner",
+                data={"registered_projects": runners.keys()},
+            )
+        ).json()
+    runners.pop(project)
+    return OsmosisUnregisterResult(removed=project, projects=runners.keys())
 
 
 @route(["/health", "/api/health"], methods="GET")
