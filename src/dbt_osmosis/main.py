3a2fec07c9c6e7ef531a0f2c407557d9ef03fb7e
Barry Hart <barrywhart@yahoo.com>
2022-10-13 08:30:28 -0400

    Raise an error at server startup if sqlfluff-templater-dbt is installed

--- src/dbt_osmosis/main.py
+++ src/dbt_osmosis/main.py
@@ -1,4 +1,5 @@
 import functools
+import importlib.util
 import multiprocessing
 import subprocess
 import sys
@@ -300,6 +301,10 @@ def serve(
 ):
     """Runs a lightweight server compatible with dbt-power-user and convenient for interactively
     running or compile dbt SQL queries with two simple endpoints accepting POST messages"""
+    if importlib.util.find_spec('sqlfluff_templater_dbt'):
+        raise ValueError("sqlfluff-templater-dbt is not compatible with dbt-osmosis server. "
+                         "Please uninstall it to continue.")
+
     logger().info(":water_wave: Executing dbt-osmosis\n")
 
     def _register_project():
