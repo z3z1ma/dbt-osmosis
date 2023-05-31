fe2e294231045d005f2335b6ddc68514eba224fd
ugmuka <ugmuka1@gmail.com>
2023-05-31 20:50:17 +0900

    fix: fixed the method of obtaining upstream knowledge in case the key is lowercase

--- src/dbt_osmosis/core/osmosis.py
+++ src/dbt_osmosis/core/osmosis.py
@@ -983,7 +983,7 @@ class DbtYamlManager(DbtProject):
         inheritables = ("description", "tags", "meta")
         changes_committed = 0
         for column in undocumented_columns:
-            prior_knowledge = knowledge.get(column, {})
+            prior_knowledge = knowledge.get(column, False) or knowledge.get(column.lower(), False) or {}
             progenitor = prior_knowledge.pop("progenitor", "Unknown")
             prior_knowledge = {k: v for k, v in prior_knowledge.items() if k in inheritables}
             if not prior_knowledge:
