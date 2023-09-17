61fe55774e98a7b674f65aa9fdbae8e99400ff60
Yasuhisa Yoshida <syou6162@gmail.com>
2023-09-17 16:23:53 +0900

    Add test for get_prior_knowledge

---
+++ tests/test_osmosis.py
@@ -0,0 +1,40 @@
+from dbt.contracts.graph.manifest import Manifest
+from dbt_osmosis.core.osmosis import DbtYamlManager
+
+
+class TestDbtYamlManager:
+    def test_get_prior_knowledge(test):
+        knowledge = {
+            "myColumn": {
+                "progenitor": "source.my_model.source.Order",
+                "generation": "generation_0",
+                "name": "my_column",
+            },
+            "my_column": {
+                "progenitor": "model.my_model.mart.Order",
+                "generation": "generation_0",
+                "name": "my_column",
+            },
+        }
+        assert (
+            DbtYamlManager.get_prior_knowledge(knowledge, "my_column")["progenitor"]
+            == "source.my_model.source.Order"
+        )
+
+    def test_get_prior_knowledge_with_camel_case(test):
+        knowledge = {
+            "myColumn": {
+                "progenitor": "model.my_model.dwh.Order",
+                "generation": "generation_1",
+                "name": "myColumn",
+            },
+            "my_column": {
+                "progenitor": "model.my_model.mart.Order",
+                "generation": "generation_0",
+                "name": "my_column",
+            },
+        }
+        assert (
+            DbtYamlManager.get_prior_knowledge(knowledge, "my_column")["progenitor"]
+            == "model.my_model.dwh.Order"
+        )
