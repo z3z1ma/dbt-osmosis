061ad04350cad9195d9a6f021e8e65677cae8bc2
Denis Kokorin <kokorin86@gmail.com>
2024-06-14 11:55:11 +0300

    More tests and more fixes

--- src/dbt_osmosis/vendored/dbt_core_interface/project.py
+++ src/dbt_osmosis/vendored/dbt_core_interface/project.py
@@ -339,6 +339,7 @@ class DbtTaskConfiguration:
         self.defer_state: Path = kwargs.get("defer_state", None)
         self.exclude_resource_types: List[str] = kwargs.get("exclude_resource_types", None)
         self.selector: str = kwargs.get("selector", None)
+        self.write_json: bool = kwargs.get("write_json", False)
 
     @classmethod
     def from_runtime_config(cls, config: RuntimeConfig, **kwargs: Any) -> "DbtTaskConfiguration":
