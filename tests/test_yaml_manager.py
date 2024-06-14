a204f698303d1a6492c72b7ff7e3d6e0a1946f13
Denis Kokorin <kokorin86@gmail.com>
2024-06-14 06:59:52 +0300

    Use pytest fixture

--- tests/test_yaml_manager.py
+++ tests/test_yaml_manager.py
@@ -1,12 +1,20 @@
+import pytest
+
 from dbt_osmosis.core.osmosis import DbtYamlManager
 
-manager = DbtYamlManager(project_dir="demo_duckdb", profiles_dir="demo_duckdb", dry_run=True)
 
-def test_list():    
-    manager.list()
+@pytest.fixture
+def yaml_manager():
+    return DbtYamlManager(project_dir="demo_duckdb", profiles_dir="demo_duckdb", dry_run=True)
 
-def test_bootstrap_sources():
-    manager.bootstrap_sources()
 
-def test_draft_project_structure_update_plan():
-    manager.draft_project_structure_update_plan()
+def test_list(yaml_manager):
+    yaml_manager.list()
+
+
+def test_bootstrap_sources(yaml_manager):
+    yaml_manager.bootstrap_sources()
+
+
+def test_draft_project_structure_update_plan(yaml_manager):
+    yaml_manager.draft_project_structure_update_plan()
